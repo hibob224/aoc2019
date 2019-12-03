1c7522bf4fe498f640bf5f0a773e9a29e2408c83
Jonathan Miller <contact@jmiller.xyz>
2019-12-03 09:39:33 +0000

    Day 3

---
+++ src/main/kotlin/day03/Day3.kt
@@ -0,0 +1,58 @@
+package day03
+
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    println("Part one: ${Day3.solvePartOne()}")
+    println("Parse two: ${Day3.solvePartTwo()}")
+}
+
+object Day3 {
+
+    private val lineOne = mutableListOf(Point(0, 0))
+    private val lineTwo = mutableListOf(Point(0, 0))
+
+    init {
+        val lineOneInput = parseInput()[0]
+        val lineTwoInput = parseInput()[1]
+        lineOneInput.forEach { lineOne.addAll(lineOne.last().move(it)) }
+        lineTwoInput.forEach { lineTwo.addAll(lineTwo.last().move(it)) }
+        lineOne.removeAt(0)
+        lineTwo.removeAt(0)
+    }
+
+    private fun parseInput(): List<List<String>> = File("src/main/kotlin/day03/input.txt").readLines().map { it.split(",") }
+
+    fun solvePartOne(): String = lineOne.intersect(lineTwo).map { it.manhattan() }.min().toString()
+
+    fun solvePartTwo(): String = lineOne.intersect(lineTwo).map { intersect ->
+        lineOne.first { intersect == it }.steps + lineTwo.first { intersect == it }.steps
+    }.min().toString()
+
+    data class Point(val x: Int, val y: Int) {
+
+        var steps = 0
+
+        fun move(movement: String): List<Point> {
+            val direction = movement.first()
+            val steps = movement.substring(1).toInt()
+
+            return (1..steps).map { step ->
+                when (direction) {
+                    'R' -> copy(x = x + step)
+                    'L' -> copy(x = x - step)
+                    'U' -> copy(y = y - step)
+                    'D' -> copy(y = y + step)
+                    else -> throw IllegalArgumentException("Unknown movement type")
+                }.also { it.steps += this.steps + step }
+            }
+        }
+
+        fun manhattan(): Int = abs(x) + abs(y)
+
+        override fun toString(): String {
+            return "${super.toString()} - $steps"
+        }
+    }
+}
---
+++ src/test/kotlin/Day3Test.kt
@@ -0,0 +1,17 @@
+import day03.Day3
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class Day3Test {
+
+    @Test
+    fun solvePartOne() {
+        assertEquals("446", Day3.solvePartOne())
+    }
+
+    @Test
+    fun solvePartTwo() {
+        assertEquals("9006", Day3.solvePartTwo())
+    }
+}
