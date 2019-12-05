5f019af345e56578f59dc3e6211d0130779ce58b
Jonathan Miller <contact@jmiller.xyz>
2019-12-05 10:13:31 +0000

    Day 5 slightly better
    Really need to redo this before more opcode questions, but I beat Dave on the leaderboard so who cares

--- src/main/kotlin/day05/Day5.kt
+++ src/main/kotlin/day05/Day5.kt
@@ -4,10 +4,8 @@ import utils.then
 import java.io.File
 
 fun main() {
-//    Day5.solvePartOne()
-    Day5.solvePartTwo()
-//    println("Part one: ${Day5.solvePartOne()}")
-//    println("Parse two: ${Day5.solvePartTwo()}")
+    println("Part one: ${Day5.solvePartOne()}")
+    println("Parse two: ${Day5.solvePartTwo()}")
 }
 
 object Day5 {
@@ -17,37 +15,22 @@ object Day5 {
         .split(",")
         .map { it.toInt() }
 
-    //7988899
-    fun solvePartOne(): String {
-        val systemId = 1
+    fun solvePartOne(): String = doOperation(1)
+
+    fun solvePartTwo(): String = doOperation(5)
+
+    private fun doOperation(input: Int): String {
         val data = parseInput().toMutableList()
         val outputs = mutableListOf<Int>()
         var position = 0
 
         do {
-            val opcode = data[position].toString()
-            var parsedOpcode = -1
-            val modes = mutableListOf(0, 0, 0)
-
-            if (opcode.length <= 2) {
-                parsedOpcode = opcode.toInt()
-            } else {
-                parsedOpcode = opcode.substring(opcode.lastIndex.dec()).toInt()
-                when (opcode.length) {
-                    5 -> {
-                        modes[0] = opcode[0].toString().toInt()
-                        modes[1] = opcode[1].toString().toInt()
-                        modes[2] = opcode[2].toString().toInt()
-                    }
-                    4 -> {
-                        modes[1] = opcode[0].toString().toInt()
-                        modes[2] = opcode[1].toString().toInt()
-                    }
-                    3 -> {
-                        modes[2] = opcode[0].toString().toInt()
-                    }
-                }
-            }
+            val opcode = data[position].toString().padStart(5, '0')
+            val parsedOpcode = opcode.substring(opcode.lastIndex.dec()).toInt()
+            val modes = mutableListOf(
+                opcode[0].toString().toInt(),
+                opcode[1].toString().toInt(),
+                opcode[2].toString().toInt())
 
             when (parsedOpcode) {
                 1 -> {
@@ -82,7 +65,7 @@ object Day5 {
                 }
                 3 -> {
                     val out = data[position.inc()]
-                    data[out] = systemId
+                    data[out] = input
                     position += 2
                 }
                 4 -> {
@@ -91,90 +74,7 @@ object Day5 {
                     } else {
                         data[position.inc()]
                     }
-                    println(out)
-                    position += 2
-                }
-                99 -> return ""
-                else -> throw IllegalStateException("Unknown opcode $opcode")
-            }
-        } while (true)
-    }
-
-    //<14217502
-    fun solvePartTwo(): String {
-        val systemId = 5
-        val data = parseInput().toMutableList()
-//        val data = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99".split(",").map { it.toInt() }.toMutableList()
-        var position = 0
-
-        do {
-            val opcode = data[position].toString()
-            var parsedOpcode = -1
-            val modes = mutableListOf(0, 0, 0)
-
-            if (opcode.length <= 2) {
-                parsedOpcode = opcode.toInt()
-            } else {
-                parsedOpcode = opcode.substring(opcode.lastIndex.dec()).toInt()
-                when (opcode.length) {
-                    5 -> {
-                        modes[0] = opcode[0].toString().toInt()
-                        modes[1] = opcode[1].toString().toInt()
-                        modes[2] = opcode[2].toString().toInt()
-                    }
-                    4 -> {
-                        modes[1] = opcode[0].toString().toInt()
-                        modes[2] = opcode[1].toString().toInt()
-                    }
-                    3 -> {
-                        modes[2] = opcode[0].toString().toInt()
-                    }
-                }
-            }
-
-            when (parsedOpcode) {
-                1 -> {
-                    val out = data[position.inc().inc().inc()]
-                    val inp1 = if (modes[2] == 0) {
-                        data[data[position.inc()]]
-                    } else {
-                        data[position.inc()]
-                    }
-                    val inp2 = if (modes[1] == 0) {
-                        data[data[position.inc().inc()]]
-                    } else {
-                        data[position.inc().inc()]
-                    }
-                    data[out] = inp1 + inp2
-                    position += 4
-                }
-                2 -> {
-                    val out = data[position.inc().inc().inc()]
-                    val inp1 = if (modes[2] == 0) {
-                        data[data[position.inc()]]
-                    } else {
-                        data[position.inc()]
-                    }
-                    val inp2 = if (modes[1] == 0) {
-                        data[data[position.inc().inc()]]
-                    } else {
-                        data[position.inc().inc()]
-                    }
-                    data[out] = inp1 * inp2
-                    position += 4
-                }
-                3 -> {
-                    val out = data[position.inc()]
-                    data[out] = systemId
-                    position += 2
-                }
-                4 -> {
-                    val out = if (modes[2] == 0) {
-                        data[data[position.inc()]]
-                    } else {
-                        data[position.inc()]
-                    }
-                    println(out)
+                    outputs.add(out)
                     position += 2
                 }
                 5 -> {
@@ -241,7 +141,7 @@ object Day5 {
                     data[out] = (inp1 == inp2) then 1 ?: 0
                     position += 4
                 }
-                99 -> return ""
+                99 -> return outputs.last().toString()
                 else -> throw IllegalStateException("Unknown opcode $opcode")
             }
         } while (true)
---
+++ src/test/kotlin/Day5Test.kt
@@ -0,0 +1,17 @@
+import day05.Day5
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class Day5Test {
+
+    @Test
+    fun solvePartOne() {
+        assertEquals("7988899", Day5.solvePartOne())
+    }
+
+    @Test
+    fun solvePartTwo() {
+        assertEquals("13758663", Day5.solvePartTwo())
+    }
+}
