6b644a2643249ec61cf629eac9e48f24da5ca03e
Jonathan Miller <contact@jmiller.xyz>
2019-12-06 10:54:51 +0000

    Day 6

---
+++ src/main/kotlin/day06/Day6.kt
@@ -0,0 +1,62 @@
+package day06
+
+import java.io.File
+
+fun main() {
+    println("Part one: ${Day6.solvePartOne()}")
+    println("Parse two: ${Day6.solvePartTwo()}")
+}
+
+object Day6 {
+
+    private fun parseInput(): Map<String, List<String>> = File("src/main/kotlin/day06/input.txt")
+        .readLines()
+        .map { it.split(")")[0] to it.split(")")[1] }
+        .groupBy({ it.first }) { it.second }
+
+    fun solvePartOne(): String {
+        val input = parseInput()
+        var count = 0
+
+        input.forEach {
+            count += it.value.size
+            val multiplier = it.value.size
+            var target = it.key
+            while (true) {
+                input.entries.find { it.value.contains(target) }?.let {
+                    count += 1 * multiplier
+                    target = it.key
+                } ?: break
+            }
+        }
+
+        return count.toString()
+    }
+
+    fun solvePartTwo(): String {
+        val input = parseInput()
+        val youLoc = input.entries.find { it.value.contains("YOU") }!!.key
+        val santaLoc = input.entries.find { it.value.contains("SAN") }!!.key
+
+        val steps = mutableListOf(listOf(youLoc))
+
+        while (true) {
+            var newFrontline = mutableListOf<String>()
+
+            newFrontline.addAll(steps.last().flatMap { input[it].orEmpty() }) // Add all objects that orbit our current objects
+            newFrontline.addAll(steps.last().mapNotNull { loc -> // Add all our direct orbits
+                input.entries.find { it.value.contains(loc) }?.key
+            })
+            newFrontline = newFrontline.filterNot { steps.flatten().contains(it) }.toMutableList() // Make sure we're not checking locations we've checked before
+            newFrontline = newFrontline.distinct().toMutableList() // Remove duplicates
+            steps.add(newFrontline)
+            if (newFrontline.contains(santaLoc)) {
+                // We've found Santa!
+                break
+            }
+        }
+
+        // Decrease steps by 1 because we don't count the starting location
+        return steps.size.dec().toString()
+    }
+}
---
+++ src/test/kotlin/Day6Test.kt
@@ -0,0 +1,17 @@
+import day06.Day6
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class Day6Test {
+
+    @Test
+    fun solvePartOne() {
+        assertEquals("294191", Day6.solvePartOne())
+    }
+
+    @Test
+    fun solvePartTwo() {
+        assertEquals("424", Day6.solvePartTwo())
+    }
+}
