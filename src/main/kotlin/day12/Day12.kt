429f1744d6b949ac12ee9a2659cd55bf881af87b
Jonathan Miller <contact@jmiller.xyz>
2019-12-12 17:03:02 +0000

    Day 12 Part 1

---
+++ src/main/kotlin/day12/Day12.kt
@@ -0,0 +1,91 @@
+package day12
+
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    println("Part one: ${Day12.solvePartOne()}")
+    println("Part two: ${Day12.solvePartTwo()}")
+}
+
+object Day12 {
+
+    private val parseRegex = "<x=(-?\\d+), y=(-?\\d+), z=(-?\\d+)>".toRegex()
+
+    private fun parseInput(): List<Moon> =
+        File("src/main/kotlin/day12/input.txt").readLines()
+            .map {
+                val (x, y, z) = parseRegex.find(it)!!.destructured
+                Moon(x.toInt(), y.toInt(), z.toInt())
+            }
+
+    //8362
+    fun solvePartOne(): String {
+        val moons = parseInput()
+        val pairs = listOf(
+            moons[0] to moons[1],
+            moons[0] to moons[2],
+            moons[0] to moons[3],
+            moons[1] to moons[2],
+            moons[1] to moons[3],
+            moons[2] to moons[3])
+
+        repeat(1000) {
+            pairs.forEach {
+                it.first.updateVelocity(it.second)
+            }
+            moons.forEach { it.applyVelocity() }
+        }
+
+        return moons.sumBy { it.energy() }.toString()
+    }
+
+    fun solvePartTwo(): String {
+        return ""
+    }
+}
+
+data class Moon(
+    var x: Int,
+    var y: Int,
+    var z: Int,
+    var xVelocity: Int = 0,
+    var yVelocity: Int = 0,
+    var zVelocity: Int = 0) {
+
+    fun updateVelocity(other: Moon) {
+        if (other.x < x) {
+            other.xVelocity += 1
+            xVelocity -= 1
+        } else if (other.x > x) {
+            other.xVelocity -= 1
+            xVelocity += 1
+        }
+        if (other.y < y) {
+            other.yVelocity += 1
+            yVelocity -= 1
+        } else if (other.y > y) {
+            other.yVelocity -= 1
+            yVelocity += 1
+        }
+        if (other.z < z) {
+            other.zVelocity += 1
+            zVelocity -= 1
+        } else if (other.z > z) {
+            other.zVelocity -= 1
+            zVelocity += 1
+        }
+    }
+
+    fun applyVelocity() {
+        x += xVelocity
+        y += yVelocity
+        z += zVelocity
+    }
+
+    fun standStill(): Boolean = (xVelocity == 0 && yVelocity == 0 && zVelocity == 0)
+
+    fun energy(): Int = (abs(x) + abs(y) + abs(z)) * (abs(xVelocity) + abs(yVelocity) + abs(zVelocity))
+
+    override fun toString(): String = "pos=<x=$x, y=$y, z=$z>, vel=<x=$xVelocity, y=$yVelocity, z=$zVelocity>"
+}
